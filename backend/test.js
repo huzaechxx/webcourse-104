4b9d2c160f9424a65d2116807907a5fd725ff4d9
huzaechxx <huzaifazahid87@gmail.com>
2024-05-01 22:06:38 +0500

    nodework

--- backend/test.js
+++ backend/test.js
@@ -1,9 +1,8 @@
 const express = require("express");
 let server = express();
+
+const router = require("./router/route.js")
+server.listen(3000);
 server.set("view engine","ejs");
 server.use(express.static("public"));
-server.get("/",function(req,res){
-    // res.send("shadi mubarak barum");
-    // res.send({name : "barum",status:"married"});
-})
-server.listen(3000);
+server.use("/",router);
---
+++ swiftspaces/app.js
@@ -0,0 +1,1 @@
+console.log("sara hehe");
